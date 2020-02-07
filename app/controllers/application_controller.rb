f111f74d5cd6c67f8e5ab4bfe2fd47d5dd5bf2ab
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-02-07 04:28:48 +0000

    making changes to the welcome view page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,13 +22,14 @@ class ApplicationController < Sinatra::Base
   
   
   helpers do 
+      
+    def current_user(session)
+      @user = User.find_by_id(session[:user_id])
+    end 
     
-  def current_user(session)
-    @user = User.find_by_id(session[:user_id])
+    def is_logged_in?(session)
+      !!session[:user_id]
+    end 
   end 
   
-  def is_logged_in?(session)
-    !!session[:user_id]
-  end 
-
-end
+  end
