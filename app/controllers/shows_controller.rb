09e4b6954e9a20322ba6a0ad2fa5193b1d70fb74
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-02-12 05:13:46 +0000

    created patch route in my show controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -53,8 +53,25 @@ class ShowsController < ApplicationController
   end
 
   patch "/shows/:id" do
-    redirect "/shows/:id"
-  end
+    if logged_in?
+      if params[:title] == "" || params[:rating] == "" || params[:streaming_service] == "" || params[:review] == ""
+        redirect to "/shows/#{params[:id]}/edit"
+      else
+        @show = Show.find_by_id(params[:id])
+          if @show && @show.user == current_user
+            if @show.update(title: params[:title], rating: params[:rating], streaming_service: params[:streaming_service], review: params[:review])
+              redirect to "/shows/#{@show.id}"
+            else
+              redirect to "/shows/#{@shows.id}/edit"
+            end
+          else
+            redirect to '/shows'
+          end
+        end
+      else
+        redirect to '/login'
+      end
+    end
 
   delete "/shows/:id/delete" do
     redirect "/shows"
